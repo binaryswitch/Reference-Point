dacc929641a46cb504f0ddf56e8882e2213d4f46
BinarySwitch <binaryswitch@jwhite.co.nz>
2016-09-21 19:12:35 +1200

    Initial project commit.

---
+++ ReferencePoint/ReferencePoint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReferencePoint
+//
+//  Created by Justin White on 21/09/16.
+//  Copyright © 2016 binaryswitch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ReferencePoint/ReferencePointTests/ReferencePointTests.m
@@ -0,0 +1,39 @@
+//
+//  ReferencePointTests.m
+//  ReferencePointTests
+//
+//  Created by Justin White on 21/09/16.
+//  Copyright © 2016 binaryswitch. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ReferencePointTests : XCTestCase
+
+@end
+
+@implementation ReferencePointTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
